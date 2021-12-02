c3fbc2c98558d2a7c89a6ebc65a03352287f86d1
gabrielcoder247 <gabrielcoder247@gmail.com>
2021-12-02 09:48:18 +0100

    Add navigation to app.js file

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 
-import { joinMission } from "Redux/Missions";
+import { joinMission } from "../Redux/Missions";
 
 const Missions = () => {
   const dispatch = useDispatch();
