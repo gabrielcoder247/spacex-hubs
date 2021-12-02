707f8dc24587f66bfb7cad03ddc7ab8ae25757d2
Mucyosoda <mucyosofa@gmail.com>
2021-12-02 14:55:55 +0100

    fix eslint error

--- src/Redux/Rockets.js
+++ src/Redux/Rockets.js
@@ -9,29 +9,29 @@ import {
 
 const initialState = { rockets: null };
 export default function rocketsReducer(state = initialState, action) {
-    switch (action.type) {
-      case GET_ROCKETS:
-        return state;
-      case SET_ROCKETS:
-        return { ...state, rockets: action.payload };
-      case SET_RESERVES:
-        const newState = state.rockets.map((rocket) => {
-          if (rocket.id !== action.payload) {
-            return rocket;
-          }
-          return { ...rocket, reserved: true };
-        });
-        return { ...state, rockets: newState };
-      case CANCEL_RESERVES:
-        const newRocketsState = state.rockets.map((rocket) => {
-          if (rocket.id !== action.payload) {
-            return rocket;
-          }
-          return { ...rocket, reserved: false };
-        });
-        return { ...state, rockets: newRocketsState };
-  
-      default:
-        return state;
-    }
+  switch (action.type) {
+    case GET_ROCKETS:
+      return state;
+    case SET_ROCKETS:
+      return { ...state, rockets: action.payload };
+    case SET_RESERVES:
+      const newState = state.rockets.map((rocket) => {
+        if (rocket.id !== action.payload) {
+          return rocket;
+        }
+        return { ...rocket, reserved: true };
+      });
+      return { ...state, rockets: newState };
+    case CANCEL_RESERVES:
+      const newRocketsState = state.rockets.map((rocket) => {
+        if (rocket.id !== action.payload) {
+          return rocket;
+        }
+        return { ...rocket, reserved: false };
+      });
+      return { ...state, rockets: newRocketsState };
+
+    default:
+      return state;
   }
+}
