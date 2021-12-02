5a4164b98d6f1d0f71376ecc12a54ae43ebf9705
Mucyosoda <mucyosofa@gmail.com>
2021-12-02 14:27:18 +0100

    create set rockets

--- src/Redux/Rockets.js
+++ src/Redux/Rockets.js
@@ -1,23 +1,5 @@
-/* eslint-disable import/prefer-default-export */
-import axios from 'axios';
-import { GET_ROCKETS, SET_ROCKETS, ERROR_ROCKETS } from './types';
-
-export const getRocketsAction = () => async (dispatch) => {
-  const baseUrl = 'https://api.spacexdata.com/v3/rockets';
-  try {
-    dispatch({ type: GET_ROCKETS });
-    const rockets = await axios.get(`${baseUrl}`);
-    dispatch({ type: SET_ROCKETS, payload: rockets });
-  } catch (error) {
-    dispatch({ type: ERROR_ROCKETS, payload: error.message });
-  }
-};
-
-export const GET_ROCKETS = 'space-travelers-hub/rockets/GET_ROCKETS';
-export const SET_ROCKETS = 'space-travelers-hub/rockets/SET_ROCKETS';
-export const ERROR_ROCKETS = 'space-travelers-hub/rockets/ERROR_ROCKETS';
-
-
+export const GET_ROCKETS = "space-travelers-hub/rockets/GET_ROCKETS";
+export const SET_ROCKETS = "space-travelers-hub/rockets/SET_ROCKETS";
+export const ERROR_ROCKETS = "space-travelers-hub/rockets/ERROR_ROCKETS";
 
 // const rockets = () => {};
-
